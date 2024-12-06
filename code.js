91546450fd2c2a598471c002b4f1a606ec4c4209
jadecraft <jadecraft9@gmail.com>
2024-12-05 21:17:17 -0500

    update

---
+++ code.js
@@ -0,0 +1,13 @@
+
+// 1. Create a string saying, "You have received this message because you have been chosen to open an important vault"
+// 2. Assign three variable. Each eqaul one of the three codes. 
+// 3. Add somments to explain the steps.
+// 4. Create a dialog box displaying the vault code.
+
+
+const user = "You have received this message because you have been chosen to open an important vault."
+const one = 5+5
+const two = 60-20
+const three = 390/10
+alert (user)
+alert (one + "-" + two + "-" + three)
