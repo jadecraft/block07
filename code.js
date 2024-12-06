50b8782371681bdadf98053c881d2a0291c5e164
jadecraft <jadecraft9@gmail.com>
2024-12-05 21:23:43 -0500

    update

--- code.js
+++ code.js
@@ -4,10 +4,14 @@
 // 3. Add somments to explain the steps.
 // 4. Create a dialog box displaying the vault code.
 
-
+//The user will see the text in the quotation:
 const user = "You have received this message because you have been chosen to open an important vault."
+//The user will see the solution to the addition problem:
 const one = 5+5
+//The user will see the solution to the subtraction problem:
 const two = 60-20
+//The user will see the solution to the division problem:
 const three = 390/10
+//The alerts below will enable the user to see the message and solutions to the equations. 
 alert (user)
 alert (one + "-" + two + "-" + three)
